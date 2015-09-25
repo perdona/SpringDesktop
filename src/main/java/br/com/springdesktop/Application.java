6f54030432649f8b31608a3c8ebf224a480e716b
Murilo da Silva <murilo@pinion.com.br>
2015-09-25 17:51:01 -0300

    Flyway integration

--- src/main/java/br/com/springdesktop/Application.java
+++ src/main/java/br/com/springdesktop/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	
     public static void main(String[] args) {
     	SpringApplication app = new SpringApplication(SpringDesktop.class);
-        app.setShowBanner(false);
+//        app.setShowBanner(false);
         app.setHeadless(false);
         app.run(args);
     }
---
+++ src/main/java/br/com/springdesktop/domain/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.springdesktop.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
