79a9cf14919eecd9e6af162493b276632b120823
Murilo da Silva <murilobr@gmail.com>
2015-09-25 21:07:24 -0300

    Flyway integration with spring boot

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ mvn spring-boot:run
 
 
 https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-data-jpa/src/main/java/sample/data/jpa
+
+http://hsqldb.org/doc/2.0/guide/index.html
+http://hsqldb.org/doc/2.0/guide/sqlgeneral-chapt.html#sgc_data_type_guide
+http://www.hsqldb.org/doc/guide/ch09.html
+http://www.hsqldb.org/doc/guide/dbproperties-chapt.html
--- src/main/java/br/com/springdesktop/Application.java
+++ src/main/java/br/com/springdesktop/Application.java
@@ -1,13 +1,18 @@
 package br.com.springdesktop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	public void run(String... args) {
+		new SpringDesktop();
+    }
 	
     public static void main(String[] args) {
-    	SpringApplication app = new SpringApplication(SpringDesktop.class);
+    	SpringApplication app = new SpringApplication(Application.class);
 //        app.setShowBanner(false);
         app.setHeadless(false);
         app.run(args);
